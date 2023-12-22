a57faae6fd080fb6f58e61bdbf569470660533a2
Hari Kiran <113755124+harikiran5184@users.noreply.github.com>
2023-12-23 02:55:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const axios = require('axios')
+const bodyParser = require('body-parser');
+const Seedr=require('seedr');
+const seedr=new Seedr()
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+async function getdata(){
+  await seedr.getVideos().then(async (res)=>{
+    if(res.length==0){
+      getdata()
+    }
+    else{
+      return res
+    }
+  })
+}
+
+app.post('/',async (req,res)=>{
+  const {magnet,auth}=req.body
+  const data=JSON.parse(auth)
+    await seedr.login(data.email,data.password);
+    console.log(JSON.parse(auth))
+    // await seedr.addMagnet("magnet:?xt=urn:btih:0f6d62c4c5aa5d296ed9efad13489bc6efaf4c7d&dn=www.5MovieRulz.top%20-%20Animal%20(2023)%20Telugu%20DVDScr%20x264%20AAC%20300MB.mkv&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fp4p.arenabg.com%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.torrent.eu.org%3a451%2fannounce&tr=udp%3a%2f%2ftracker.dler.org%3a6969%2fannounce&tr=udp%3a%2f%2fopen.stealth.si%3a80%2fannounce&tr=udp%3a%2f%2fopentracker.i2p.rocks%3a6969%2fannounce&tr=http%3a%2f%2ftracker.gbitt.info%3a80%2fannounce&tr=udp%3a%2f%2ftracker.tiny-vps.com%3a6969%2fannounce&tr=udp%3a%2f%2fmovies.zsw.ca%3a6969%2fannounce");
+    // Starts downloading, wait till that happens
+    var contents = await seedr.getVideos();
+    let ne;
+    
+    if(contents.length==0){
+      await seedr.addMagnet(magnet).finally(()=>{
+        console.log("adding data....")
+        res.json(true)
+      })
+      
+    }
+    else{
+      return res.json(false)
+    }
+    
+})
+app.post('/del',async (req,res)=>{
+  const {auth}=req.body
+  console.log(JSON.parse(auth))
+  const data = JSON.parse(auth)
+  await seedr.login(data.email,data.password);
+  var contents = await seedr.getVideos();
+  if(contents.length>0){
+    // var ne = await seedr.getFile(contents[0][0].id)
+    console.log("deleting......")
+    await seedr.deleteFolder(contents[0][0].fid).then(()=>{
+      console.log("deleted....")
+      return res.json({len:true})
+    })
+    
+  }
+  else{
+    return res.json({len:true})
+  }
+  
+  // await seedr.deleteFolder(contents[0][0].fid)
+})
+app.post('/fetchVideo',async (req,res)=>{
+  const{auth}=req.body
+  const data=JSON.parse(auth)
+  await seedr.login(data.email,data.password);
+  var contents = await seedr.getVideos();
+  console.log(contents)
+  if(contents.length>0){
+    console.log("fetching......")
+    let n=await seedr.getFile(contents[0][0].id).then((e)=>{
+      console.log("fetched....")
+      console.log(e)
+      return res.json(e)
+    })
+    
+  }
+  else{
+    return res.json(contents.length)
+  }
+})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
