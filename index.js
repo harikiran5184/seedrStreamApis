f5ae1773fada2ad0c3b7f748f73db491c46b2124
Hari Kiran <113755124+harikiran5184@users.noreply.github.com>
2024-03-12 15:55:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -154,8 +154,8 @@ app.get('/adminmovie/:id', (req, res) => {
 });
 
 app.get('/version',(req,res)=>{
-  const version="4.0.0";
-  const link="https://drive.google.com/file/d/19-g9ekPN9MiGji3dysrqYWYDSG4VxG5x/view?usp=drivesdk";
+  const version="4.0.1";
+  const link="https://drive.google.com/file/d/1hu5CuZyF2Yvn9hDj1Yxz4k6dhvioYZFd/view?usp=sharing";
   const whatsnews=['changes in bottom bar','Make it more responsive for some devices']
   res.json({version:version,link:link,whatsnews:whatsnews})
 })
